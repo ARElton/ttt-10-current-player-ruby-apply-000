d5b40ea9c9c51328b10698b0f555cf264195a11c
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-12 18:23:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0 
   
   board.each do |turn|
-   if turn == "X" || turn == "O"
+   if board == "X" || board == "O"
      counter += 1
      return "#{counter}"
    end
