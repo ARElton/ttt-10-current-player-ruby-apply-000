1dbd54db73b93c0c0605a9e39e8b5a2c6a014a27
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-12 18:01:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,5 @@
 def turn_count(board)
-  
   counter = 0 
-  board = [" ", " ", " ", " ", " ", " ", " ", " "]
-  new_board = [ ]
   
   board.each do |occupied_positions|
    new_board << counter += 1
